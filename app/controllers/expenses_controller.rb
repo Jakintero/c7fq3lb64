b7732c152e2dcd85794d3d6b8ea39efc7e9da2b0
Javier Quintero <jakintero@hotmail.com>
2017-11-29 21:29:57 -0500

    filtrando2

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,11 +1,27 @@
 class ExpensesController < ApplicationController
   def index
     @expenses = Expense.order("date DESC")
-    if params[:concept].present?
-      @expenses = @expenses.where("concept ILIKE ?", "%#{params[:concept]}%")
-    end
-    if params[:category_id].present?
+
+    if params[:concept].present? && params[:category_id].present?
+      @expenses = @expenses.where("concept LIKE :q", q: "%#{params[:concept]}%")
       @expenses = @expenses.where("category_id = ?", params[:category_id])
+    elsif params[:category_id].present?
+      @expenses = @expenses.where("category_id = ?", params[:category_id])
+    elsif params[:concept].present?
+      @expenses = @expenses.where("concept LIKE :q", q: "%#{params[:concept]}%")
     end
   end
 end
+
+# def index
+#     @expenses = Expense.order("date DESC")
+#
+#     if params[:concept].present? && params[:category_id].present?
+#       @expenses = @expenses.where("concept LIKE :q", q: "%#{params[:concept]}%")
+#       @expenses = @expenses.where("category_id = ?", params[:category_id])
+#     elsif params[:category_id].present?
+#       @expenses = @expenses.where("category_id = ?", params[:category_id])
+#     elsif params[:concept].present?
+#       @expenses = @expenses.where("concept LIKE :q", q: "%#{params[:concept]}%")
+#     end
+# end
